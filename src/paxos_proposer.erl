6c1745017e8ce44cdc3be88658d2b20f85799301
yfractal <yfractal@gmail.com>
2019-07-14 11:29:17 +0800

    feat: implemented basic proposer

---
+++ src/paxos_proposer.erl
@@ -0,0 +1,61 @@
+-module(paxos_proposer).
+
+-behaviour(gen_statem).
+
+%% APIp
+-export([start_link/1,
+         prepare/2]).
+
+%% gen_statem callbacks
+-export([callback_mode/0, init/1, terminate/3, code_change/4]).
+-export([prepare/3]).
+-export([proposal/3]).
+
+-define(SERVER, ?MODULE).
+
+-record(data, {id}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+start_link(Id) ->
+    gen_statem:start_link(?MODULE, [Id], []).
+
+prepare(Pid, {N, Proposal}) ->
+    gen_statem:cast(Pid, {prepare, N, Proposal}).
+%%%===================================================================
+%%% gen_statem callbacks
+%%%===================================================================
+
+callback_mode() ->
+    [state_functions, state_enter].
+
+init([Id]) ->
+    process_flag(trap_exit, true),
+    {ok, prepare, #data{id=Id}}.
+
+prepare(enter, _Msg, Data) ->
+    io:format("Enter prepare state "),
+    %% send_prepare msg to all acceptor
+    {keep_state, Data};
+prepare(cast, {prepare, _N, _Proposal}, Data) ->
+    io:format("receive prepare"),
+    {next_state, proposal, Data}.
+
+proposal(enter, _Msg, Data) ->
+    %% send_prepare msg to all acceptor
+    {keep_state, Data};
+proposal(cast, {proposal_accepted}, Data) ->
+    {keep_state, Data}.
+
+terminate(_Reason, _State, _Data) ->
+    void.
+
+
+code_change(_OldVsn, State, Data, _Extra) ->
+    {ok, State, Data}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
